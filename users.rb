a11f3e49c6c4ef9ce8991d2269850facb7b9387e
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-07-11 19:42:44 +0100

    Add Facter fact about system users.
    Adding Facter fact which provides a list of system users.
    
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ users.rb
@@ -0,0 +1,31 @@
+#
+# users.rb
+#
+
+require 'thread'
+require 'facter'
+require 'puppet'
+
+if Facter.value(:kernel) == 'Linux'
+
+  mutex = Mutex.new
+
+  users = []
+
+  Puppet::Type.type('user').instances.each do |user|
+    # Get details about the user from the corresponding instance ...
+    instance = user.retrieve
+
+    mutex.synchronize do
+      # Add user to list only if the user is not an essential system user ...
+      users << user.name unless instance[user.property(:uid)].to_i < 500
+    end
+  end
+
+  Facter.add('users') do
+    confine :kernel => :linux
+    setcode { users.sort.join(',') }
+  end
+end
+
+# vim: set ts=2 sw=2 et :
