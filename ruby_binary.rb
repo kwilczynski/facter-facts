8c77467bad835214c43c7e693fe46b2cb7ce9e77
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-12-01 00:31:22 +0000

    Add Facter fact that exposes location of the Ruby binary on the system.
    This fact will expose location of the Ruby interpreter on the underlying
    operating system.  This might be useful when Ruby binary is located
    in a different place than "/usr/bin/ruby" for whatever reason ...
    
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ ruby_binary.rb
@@ -0,0 +1,25 @@
+#
+# ruby_binary.rb
+#
+# This fact provides information about where Ruby binary is located on
+# the underlying file system ...
+#
+
+require 'facter'
+
+if Facter.value(:kernel) == 'Linux'
+  #
+  # We add our fact as single word "rubybinary" instead of "ruby_binary"
+  # purely to maintain compliance with other Ruby-related facts ...
+  #
+  Facter.add('rubybinary') do
+    confine :kernel => :linux
+    setcode do
+      config = Config::CONFIG
+      File.join(config['bindir'], config['ruby_install_name'])
+    end
+  end
+end
+
+# vim: set ts=2 sw=2 et :
+# encoding: utf-8
