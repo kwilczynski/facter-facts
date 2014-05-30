8324203fded0e01640b7dea8d694d4a1301a7e4d
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2014-05-30 19:38:48 +0100

    Fix case when the /proc/partitions contains "cciss/c0d0".
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- partitions.rb
+++ partitions.rb
@@ -81,7 +81,7 @@ if Facter.value(:kernel) == 'Linux'
     end
 
     # Convert back into a string value ...
-    disk = disk.first.to_s
+    disk = Array(disk).first.to_s
 
     # We have something rather odd that did not parse at all, so ignore ...
     next if disk.empty?
